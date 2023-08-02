ba184634b057c96ddc6c0352ce780f0b5940af03
Thato Mabe <willynough@gmail.com>
2023-08-02 12:20:26 +0200

    first commit

---
+++ migrations/002-create-greetings-table.sql
@@ -0,0 +1,4 @@
+INSERT INTO greetings (language, greeting) VALUES ('xhosa', 'molo');
+INSERT INTO greetings (language, greeting) VALUES ('tswana', 'dumela');
+INSERT INTO greetings (language, greeting) VALUES ('english', 'Hello');
+INSERT INTO greetings (language, greeting) VALUES ('zulu', 'sawubona');
